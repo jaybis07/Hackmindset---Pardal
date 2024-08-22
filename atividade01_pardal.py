ed4b6fe7423eebb0c299ecf16b3169dec5aa75fc
jaybis07 <contaale1515@gmail.com>
2024-08-22 09:10:38 -0300

    Update atividade01_pardal.py

--- atividade01_pardal.py
+++ atividade01_pardal.py
@@ -1,16 +1,16 @@
-# Nome do arquivo
-nome_arquivo = "darkweb2017-top100.txt"
+# Caminho absoluto para o arquivo, não consegui executar com o caminho sireto da pasta
+caminho_absoluto = '/Users/labsfiap/downloads/darkweb2017-top100.txt'
 
 # Tenta abrir e ler o arquivo alvo
 try:
-    with open(nome_arquivo, 'r') as arquivo:
+    with open(caminho_absoluto, 'r') as a:
         # Lê somente as 10 primeiras linhas
-        for i, linha in enumerate(arquivo):
+        for i, linha in enumerate(a):
             if i < 10:
                 print(linha, end='')
             else:
                 break
 except FileNotFoundError:
-    print(f"O arquivo '{nome_arquivo}' não foi encontrado.")
+    print(f"O arquivo '{caminho_absoluto}' não foi encontrado.")
 except IOError:
-    print(f"Ocorreu um erro ao tentar ler o arquivo '{nome_arquivo}'.")
+    print(f"Ocorreu um erro ao tentar ler o arquivo '{caminho_absoluto}'.")
